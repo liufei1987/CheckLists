3b4e420987d72e8b8cc278f815d1f5aebb8f31c9
liufeismart <liufeismart@163.com>
2019-10-31 16:28:53 +0800

    创建首页ChecklistsViewController

---
+++ Checklist/ChecklistsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by liufeismart on 2019/10/31.
+//  Copyright © 2019 liufeismart. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        let label = cell.viewWithTag(1001) as! UILabel
+        label.text = "Hello"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+            }
+            else {
+                cell.accessoryType = .checkmark
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+//    
+//    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+//        return nil
+//    }
+
+
+}
+
